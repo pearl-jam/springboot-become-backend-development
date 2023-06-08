7626a2e4be012b6c8f8645987a69d48b78ccda81
Ki-Joon PARK <glycerine@live.co.kr>
2023-06-08 14:26:14 +0900

    add: 03. 스프링 부트 3 구조 이해하기 ~ 3.2 스프리으 부트 3 프로젝트 발전시키기 (Rest 한글깨짐 설정 수정)

--- src/main/java/me/glycerine/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/glycerine/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -1,7 +1,7 @@
 package me.glycerine.springbootdeveloper;
 
-import org.springframework.boot.SpringApplication; //
-import org.springframework.boot.autoconfigure.SpringBootApplication;//
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootDeveloperApplication {
