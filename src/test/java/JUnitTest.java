126a8420e13889432c578f500dbc0f0f71867c56
Ki-Joon PARK <glycerine@live.co.kr>
2023-06-13 11:33:19 +0900

    fix: (working)

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -3,13 +3,24 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class JUnitTest {
-    @DisplayName("1 + 2? 3??") // ??? ??
+
+    @DisplayName("1 + 2? 3??")
     @Test
     public void junitTest() {
         int a = 1;
         int b = 2;
         int sum = 3;
 
-        Assertions.assertEquals(a + b, sum); // ?? ??? ??
+        Assertions.assertEquals(a + b, sum);
     }
+
+//    @DisplayName("1 + 3? 4??")
+//    @Test
+//    public void junitFailedTest() {
+//        int a = 1;
+//        int b = 3;
+//        int sum = 3;
+//
+//        Assertions.assertEquals(a + b, sum);
+//    }
 }
