000b05ac4c47972d2f1ca27ab69da085f9aab1e4
Ki-Joon PARK <glycerine@live.co.kr>
2023-06-27 11:20:54 +0900

    add: 07. 블로그 화면 구성하기 (완료) ~ 7.6 생성 기능 마무리하기

--- build/resources/main/static/js/article.js
+++ build/resources/main/static/js/article.js
@@ -1,6 +1,5 @@
 // 삭제 기능
 const deleteButton = document.getElementById('delete-btn');
-
 if (deleteButton) {
     deleteButton.addEventListener('click', event => {
         let id = document.getElementById('article-id').value;
@@ -33,6 +32,26 @@ if (modifyButton) {
         .then(() => {
             alert('수정이 완료되었습니다.');
             location.replace(`/articles/${id}`);
+        });
+    });
+}
+
+// 등록 기능
+const createButton = document.getElementById('create-btn');
+if (createButton) {
+    createButton.addEventListener('click', (event) => {
+        fetch('/api/articles', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value
+            }),
+        }).then(() => {
+            alert('등록 완료되었습니다.');
+            location.replace('/articles');
         });
     });
 }
