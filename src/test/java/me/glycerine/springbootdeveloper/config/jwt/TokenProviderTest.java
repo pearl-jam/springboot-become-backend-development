63319c1131200c22dc3d3c3063696e16c247194f
Ki-Joon PARK <glycerine@live.co.kr>
2023-07-20 15:28:08 +0900

    add: 09. JWT 로 로그인/로그아웃 구현하기 ~ 9.2.2 토큰 제공자 추가하기 (working)

---
+++ src/test/java/me/glycerine/springbootdeveloper/config/jwt/TokenProviderTest.java
@@ -0,0 +1,47 @@
+package me.glycerine.springbootdeveloper.config.jwt;
+
+import io.jsonwebtoken.Jwts;
+import me.glycerine.springbootdeveloper.domain.User;
+import me.glycerine.springbootdeveloper.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class TokenProviderTest {
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    @DisplayName("generateToken(): 유저 정보와 만료 기간을 전달해 토큰을 만들 수 있다.")
+    @Test
+    void generateToken() {
+        // given
+        User testUser = userRepository.save(User.builder()
+                .email("user@gmail.com")
+                .password("test")
+                .build());
+
+        // when
+        String token = tokenProvider.generateToken(testUser, Duration.ofDays(14));
+
+        // then
+        Long userId = Jwts.parser()
+                .setSigningKey(jwtProperties.getSecretKey())
+                .parseClaimsJws(token)
+                .getBody()
+                .get("id", Long.class);
+
+        assertThat(userId).isEqualTo(testUser.getId());
+    }
+}
